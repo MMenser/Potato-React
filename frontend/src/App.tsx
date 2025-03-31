b0b6a0643e2a22b300afb8cf990e9a451a9caa12
Mason Menser <mason.menser@wsu.edu>
2025-03-30 19:47:20 -0700

    cosmetics for 4 boxs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import './styles.css'
-import Sensor from './components/sensor'
-import axios from 'axios'
+import Box from './components/box'
 function App() {
 
   return (
-    <div className="bg-gray-600 min-h-screen flex items-center justify-start text-white">
-      <Sensor></Sensor>
+    <div className="bg-gray-800 min-h-screen flex flex-col items-start">
+      <div className='w-full h-screen flex flex-col p-10 space-y-6 text-black">'>
+      <Box id={1}></Box>
+      <Box id={2}></Box>
+      <Box id={3}></Box>
+      <Box id={4}></Box>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/box.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import DataGraph from './datagraph';
+import axios
+ from 'axios';
+
+ interface BoxProps {
+  id: number
+ }
+
+function Box({id}: BoxProps) {
+    const [averageTemperature, setAverageTemperature] = useState(0.0);
+    
+    const fetchAPI = async () => {
+        const response = await axios.get("http://127.0.0.1:8080/data")
+        setAverageTemperature(response.data.data[0])
+        console.log(response.data.data[0]);
+      };
+    
+      useEffect(() => {
+        fetchAPI()
+      },[]);
+
+    return (
+        <div className="w-full h-1/4 bg-white p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-center mb-4">Box {id}</h2>
+            <div className='flex-row flex'>
+            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
+                <p className="text-center">Average</p>
+                <p className="text-center">{averageTemperature} F</p>
+            </div>
+            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
+                <p className="text-center">Average</p>
+                <p className="text-center">{averageTemperature} F</p>
+            </div>
+            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
+                <p className="text-center">Average</p>
+                <p className="text-center">{averageTemperature} F</p>
+            </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Box;
