735ad12e11676d6a35786f787347b0bfe5504976
Mason Menser <mason.menser@wsu.edu>
2025-04-03 13:29:18 -0700

    box styling work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,26 @@
-import { useEffect, useState } from 'react'
-import './styles.css'
-import Box from './components/box'
+import { useEffect, useState } from "react";
+import "./styles.css";
+import Box from "./components/box";
 function App() {
-
   return (
-    <div className="bg-gray-800 min-h-screen flex flex-col items-start">
-      <div className='w-full h-screen flex flex-col p-10 space-y-6 text-black">'>
-      <Box id={1}></Box>
-      <Box id={2}></Box>
-      <Box id={3}></Box>
-      <Box id={4}></Box>
+    <div className="bg-black min-h-screen flex flex-row">
+      <div className="w-full flex-col">
+        {/* First Row */}
+        <div className="w-full h-1/2 flex-auto flex-row">
+          <Box id={1} />
+          <Box id={2} />
+        </div>
+      </div>
+
+      <div className="w-full flex-col">
+        {/* Second Row */}
+        <div className="w-full h-1/2 flex-auto flex-row">
+          <Box id={3} />
+          <Box id={4} />
+        </div>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- frontend/src/components/box.tsx
+++ frontend/src/components/box.tsx
@@ -1,55 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import DataGraph from './datagraph';
-import axios
- from 'axios';
+import React, { useEffect, useState } from "react";
+import DataGraph from "./datagraph";
+import axios from "axios";
 
- interface BoxProps {
-  id: number
- }
+interface BoxProps {
+  id: number;
+}
 
-function Box({id}: BoxProps) {
-    const [averageTemperature, setAverageTemperature] = useState(0.0);
-    const [ambientTemperature, setAmbientTemperature] = useState(0.0);
-    const [targetTemperature, setTargetTemperature] = useState(0.0);
-    const [currentVoltage, setCurrentVoltage] = useState(0.0);
-    
-    const fetchAPI = async () => {
-        const response = await axios.get("http://127.0.0.1:8080/data")
-        setAverageTemperature(response.data.data[0]);
-        setAmbientTemperature(response.data.data[1]);
-        setTargetTemperature(response.data.data[2]);
-        setCurrentVoltage(response.data.data[3]);
-        console.log(response.data.data);
-      };
-    
-      useEffect(() => {
-        fetchAPI()
-      },[]);
+function Box({ id }: BoxProps) {
+  const [averageTemperature, setAverageTemperature] = useState(0.0);
+  const [ambientTemperature, setAmbientTemperature] = useState(0.0);
+  const [targetTemperature, setTargetTemperature] = useState(0.0);
+  const [currentVoltage, setCurrentVoltage] = useState(0.0);
 
-    return (
-        <div className="w-full h-1/4 bg-white p-4 rounded-lg shadow-md">
-            <h2 className="text-xl font-bold text-center mb-4">Box {id}</h2>
-            <div className='flex-row flex'>
-            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
-                <p className="text-center">Average</p>
-                <p className="text-center">{averageTemperature} F</p>
-            </div>
-            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
-                <p className="text-center">Ambient</p>
-                <p className="text-center">{ambientTemperature} F</p>
-            </div>
-            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
-                <p className="text-center">Target</p>
-                <p className="text-center">{targetTemperature} F</p>
-            </div>
-            <div className="w-1/12 h-1/2 bg-gray-300 p-2 mx-auto rounded-md shadow-inner">
-                <p className="text-center">Current Voltage</p>
-                <p className="text-center">{currentVoltage} F</p>
-            </div>
-            </div>
+  const fetchAPI = async () => {
+    const response = await axios.get("http://127.0.0.1:8080/data");
+    setAverageTemperature(response.data.data[0]);
+    setAmbientTemperature(response.data.data[1]);
+    setTargetTemperature(response.data.data[2]);
+    setCurrentVoltage(response.data.data[3]);
+    console.log(response.data.data);
+  };
 
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <div className="w-full h-full p-5 bg-black border-1 border-white">
+      <h2 className="text-xl text-white font-bold text-center mb-3">
+        Box {id}
+      </h2>
+      <div className="flex flex-row">
+        <div className="flex flex-col space-y-2">
+          <div className="flex flex-row justify-start">
+            <p className="text-white w-18">Average</p>
+            <p className="text-green-500">{averageTemperature} °F</p>
+          </div>
+          <div className="flex flex-row justify-start">
+            <p className="text-white w-18">Ambient</p>
+            <p className="text-green-500">{ambientTemperature} °F</p>
+          </div>
+          <div className="flex flex-row justify-start">
+            <p className="text-white w-18">Target</p>
+            <p className="text-green-500">{targetTemperature} °F</p>
+          </div>
+          <div className="flex flex-row justify-start">
+            <p className="text-white w-18">Voltage</p>
+            <p className="text-green-500">{currentVoltage} V</p>
+          </div>
         </div>
-    );
+        <div className="mx-12">
+        <DataGraph />
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Box;
